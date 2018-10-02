567acecb6a5e2f52f703bddea26dc22197006590
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-02 12:55:04 +0300

    Add get all months feature.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <header>
       <img src="./assets/logo.png">
     </header>
-    <leftNav>
-      <p>This is the left navbar </p>
-    </leftNav>
+    <div class="leftNav">
+      <ul  v-for="month in months">
+        <li :key="month._id">{{ month.fromDate }} to {{ month.toDate }}</li>
+      </ul>
+    </div>
     <main>
       <h1>Main content</h1>
     </main>
@@ -16,11 +18,29 @@
 </template>
 
 <script>
+import axios from "axios"; 
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      months: []
+    }
+  },
+
+  mounted () {
+    this.fetchMonths();
+    console.log(this.months)
+  },
+
+  methods: {
+    fetchMonths () {
+      axios
+        .get('http://localhost:3001/api/v1/month-form')
+        .then(response => { 
+            this.months = response.data;
+            })
+        .catch(error => { this.errors.push(error) })
     }
   }
 }
