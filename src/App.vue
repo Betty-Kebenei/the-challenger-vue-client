2b679ed2201273885f8fcf6809a7d1002f113d63
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-02 12:34:11 +0300

    Add the initial project setup.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <img src="./assets/logo.png">
+    </header>
+    <leftNav>
+      <p>This is the left navbar </p>
+    </leftNav>
+    <main>
+      <h1>Main content</h1>
+    </main>
+    <footer>
+      <small> Authored by Betty </small>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style src="./styles/app.css">
+</style>
