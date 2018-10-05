d5c974aceb49e98047f0a2feb918096764bc2d62
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-05 17:05:05 +0300

    Add a skeleton of the faithfulness table.

---
+++ src/components/FaithfulnessTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Reading Speed Bar Chart</h1>
+        <table >
+            <tr>
+                <th>Day</th>
+                <td>1</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <th>Date</th>
+                <td>10/7</td>
+                <td>11/7</td>
+            </tr>
+            <tr>
+                <th>Percentage</th>
+                <td>4</td>
+                <td>7</td>
+            </tr>
+            <tr>
+                <th>Notes</th>
+                <td>No</td>
+                <td>Yes</td>
+            </tr>
+            <tr>
+                <th>SMR</th>
+                <td>Yes</td>
+                <td>Yes</td>
+            </tr>
+            <tr>
+                <th>Prayer</th>
+                <td>Yes</td>
+                <td>No</td>
+            </tr>
+            <tr>
+                <th>Data Validity</th>
+                <td>Yes</td>
+                <td>No</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'faithfullnessTable',
+    }
+</script>
+
+<style scoped src="../styles/faithfulnessTable.css">
+</style>
