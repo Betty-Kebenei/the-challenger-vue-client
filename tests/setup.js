4a51fe7052382b0eebef0d4783f3ccb8d523982f
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-09 11:31:33 +0300

    Add tests for methods in the App.vue

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,7 @@
 require('jsdom-global')()
+
+const apiURL = 'http://localhost:3001/api/v1/month-form';
  
 global.expect = require('expect')
+global.apiURL = apiURL;
 window.Date = Date; 
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,6 +1,6 @@
 import App from '../../src/App.vue';
 import {shallowMount} from '@vue/test-utils';
-import sinon from 'sinon';
+import moxios from 'moxios';
 
 describe('App', () => {
     it('should have a component named app', () => {
@@ -24,4 +24,45 @@ describe('App template', () => {
         const h1 = wrapper.find('h1');
         expect(h1.text()).toContain('Months');
     });
+});
+
+describe('App methods', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        moxios.install();
+
+        wrapper = shallowMount(App);
+    });
+
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
+    it('should set showCharts to true and month id', () => {
+        wrapper.vm.fetchId(2);
+        expect(wrapper.vm.showCharts).toBe(true)
+        expect(wrapper.vm.monthId).toBe(2)
+    });
+
+    it('should fetch all the month forms', (done) => {
+        moxios.stubRequest(`${apiURL}`, {
+            status: 200,
+            response: {
+                data: [
+                    {   
+                        fromDate: '7-5-2018',
+                        toDate: '7-6-2018',
+                        _v: 0,
+                        _id: '5b879d497f0b5daa557b33ct'
+                    }
+                ] 
+            }
+        });
+
+        moxios.wait(() => {
+            expect(wrapper.find('li').html()).toContain('strong');
+            done();
+        });
+    })
 });
