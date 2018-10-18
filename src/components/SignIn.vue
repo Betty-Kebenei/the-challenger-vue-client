0a1b3b90c59755804603df33a04e034f81b2edc8
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-18 11:03:53 +0300

    Add signin validations.

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -7,7 +7,13 @@
                     <label for="username">Email</label>
                 </div>
                 <div class="col-input">
-                    <input type="text" placeholder="Enter your email or username" id="username" v-model="username" />
+                    <input
+                        type="text" 
+                        placeholder="Enter your email" 
+                        id="username" 
+                        v-model="username" 
+                        required
+                    />
                 </div>
             </div>
             <div class="row">
@@ -15,26 +21,49 @@
                     <label for="password">Password</label>
                 </div>
                 <div class="col-input">
-                    <input type="password" id="password" v-model="password" />
+                    <input 
+                        type="password"
+                        id="password" 
+                        v-model="password" 
+                        required
+                    />
                 </div>
             </div>
             <div class="check"> 
                 <label for="remember">Remember me</label>
-                <input type="checkbox" id="remember" value="remember" />
+                <input  
+                    type="checkbox" 
+                    id="remember" 
+                    value="remember" 
+                />
             </div>
             <div class="forgot-password"> 
                 <a href="#">Forgot password?</a>
             </div>
             <br />
-            <div class="signin"><input type="submit" value="Login" /></div>
+            <div class="signin">
+                <input 
+                    type="submit" 
+                    value="Login" 
+                />
+            </div>
         </form>
         <br />
         <h2>OR</h2>
         <h3>Login with a social media account</h3>
         <div class="social-media-icons">
-            <a class="facebook"><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" />Facebook</a>
-            <a class="twitter"><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }" />Twitter</a>
-            <a class="google"><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google' }"/>Google</a>
+            <a class="facebook">
+                <font-awesome-icon 
+                    :icon="{ prefix: 'fab', iconName: 'facebook' }" 
+                />Facebook</a>
+            <a class="twitter">
+                <font-awesome-icon 
+                    :icon="{ prefix: 'fab', iconName: 'twitter' }" 
+                />Twitter</a>
+            <a class="google">
+                <font-awesome-icon 
+                    :icon="{ prefix: 'fab', iconName: 'google' }"
+                />Google</a>
         </div>
     </div>
 </template>
