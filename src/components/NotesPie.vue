0280b605c6b6dbec9be3d46f86488bd1590a2452
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-23 21:13:30 +0300

    Add a prayer pie chart.

--- src/components/NotesPie.vue
+++ src/components/NotesPie.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <h1>Notes Pie Chart</h1>
-        <canvas id="pie-chart" height="80px"></canvas>
+        <h1>Pie Charts</h1>
+
+        <h2>Notes Pie Chart</h2>
+        <canvas id="notes-pie-chart" height="80px"></canvas>
+
+        <h2>Prayer Pie Chart</h2>
+        <canvas id="prayer-pie-chart" height="80px"></canvas>
     </div>
 </template>
 
@@ -15,20 +20,24 @@ export default {
     },
 
     mounted() {
-        this.createNotesPieChart('pie-chart', this.dailies[4]);
+        this.createPieChart('notes-pie-chart', this.dailies[4]);
+        this.createPieChart('prayer-pie-chart', this.dailies[5]);
+        this.createPieChart('notes-pie-chart', this.dailies[4]);
     },
 
     methods: {
-      createNotesPieChart(chartId, dailyData){
+      createPieChart(chartId, dailyData){
             const ctx = document.getElementById(chartId);
 
             let x;
             let trueCount = 0;
             let falseCount = 0;
 
-            for(x=0; x<dailyData.length; x++) {
-                trueCount = trueCount + 1
-            }
+            dailyData.map(data => {
+                if(data === true){
+                    trueCount = trueCount + 1
+                }
+            });
 
             falseCount = dailyData.length - trueCount;
 
